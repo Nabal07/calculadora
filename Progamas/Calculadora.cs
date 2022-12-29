519fe49a1746d6c1d480a5fe709fa6d4a4f26a55
Gabriel de Santana <gabrieldesantana.contato@gmail.com>
2022-12-29 11:16:08 -0300

    Commit inicial

---
+++ Progamas/Calculadora.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace ProjetoCalc
+{
+    public class Calculadora
+    {
+        //Soma
+        public static void Soma (int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            System.Console.WriteLine($"A soma de {num1} + {num2} = {resultado}");
+        }
+        //Subtracao
+        public static void Subtracao (int num1, int num2)
+        {
+            int resultado = num1 - num2;
+            System.Console.WriteLine($"A subtracao de {num1} - {num2} = {resultado}");
+        }
+       //Divisao
+        public static void Divisao (int num1, int num2)
+        {
+            int resultado = num1 / num2;
+            System.Console.WriteLine($"A divisao de {num1} / {num2} = {resultado}");
+        }
+       //Multiplicacao
+        public static void Multiplicacao (int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            System.Console.WriteLine($"A multiplicacao de {num1} * {num2} = {resultado}");
+        }
+        //Exponeceacao
+        public static void Exponeciacao (int num1, int num2) =>
+        Console.WriteLine($"A exponeciacao de {num1} ** {num2} = {(int)Math.Pow(num1,num2)}");
+        } 
+    }
---
+++ Progamas/Program.cs
@@ -0,0 +1,50 @@
+﻿//metodo:
+using System;
+
+namespace ProjetoCalc;
+
+class Program
+{
+  static void Main(string[]args)
+  {
+    //calculadora com metodos
+
+    while(true){
+   System.Console.WriteLine("Calculadora");
+   System.Console.WriteLine("Digite o primeiro numero: ");
+   int num1 = int.Parse(Console.ReadLine());
+   System.Console.WriteLine("=======================");
+   System.Console.WriteLine("Digite o segund numero: ");
+   int num2 = int.Parse(Console.ReadLine());
+   System.Console.WriteLine("=======================");
+   System.Console.WriteLine("Cada operacao: 1-Soma, 2-Subtracao, 3-Divisao, 4-Multiplicacao, 5-Exponeciação");
+   int operacao = int.Parse(Console.ReadLine());
+   System.Console.WriteLine("=======================");
+   if(operacao == 1)
+   {
+      Calculadora.Soma(num1,num2);  
+   }
+  if(operacao == 2)
+   {
+      Calculadora.Subtracao(num1,num2);
+   }
+   if(operacao == 3)
+   {
+      Calculadora.Divisao(num1,num2);
+   }
+   if(operacao == 4)
+   {
+      Calculadora.Multiplicacao(num1,num2);
+   }
+   if(operacao == 5)
+   {
+    Calculadora.Exponeciacao(num1,num2);
+   }
+    else 
+    {
+      break;
+    }
+  }
+  }
+}
+//fim...
